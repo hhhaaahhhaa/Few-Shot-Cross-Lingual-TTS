fix protonet sign bug

lightning/model/reduction.py
@@ -143,7 +143,7 @@ class RandomSelectReductionModule(pl.LightningModule):
 
 class PoolReductionModule(pl.LightningModule):
     """
-    Given K class labels and some representations, pool one representation for each class.
+    Given K class labels and some representations, perform partial average representation for each class.
     A representation pool is maintained for each class.
     """
 
@@ -166,13 +166,14 @@ class PoolReductionModule(pl.LightningModule):
         """
         pooled_repr = []
         for c in class_labels:
-            if c not in self.pools:
-                self.pools[c] = DataPool(max_size=self.max_size, auto_resize=True)
-            self.pools[c].extend(table[c])
-            pooled = self.pools[c].choices(1)
-            if pooled is None:
+            if len(table[c]) == 0:
                 pooled_repr.append(torch.zeros(repr_shape).to(self.device))
             else:
+                self.pools[c] = DataPool(max_size=self.max_size, auto_resize=False)
+                self.pools[c].extend(table[c])
+                self.pools[c].resize()
+
+                pooled = torch.mean(torch.stack(self.pools[c]._data, axis=0), axis=0)
                 pooled_repr.append(pooled)
         pooled_repr = torch.stack(pooled_repr, dim=0).float()
 

lightning/systems/phoneme_recognition/SSLProtoNet.py
@@ -90,7 +90,7 @@ class SSLProtoNetSystem(AdaptorSystem):
         x = self.downstream(ssl_repr, labels[4].cpu())
 
         # Prototype loss
-        output = torch.linalg.norm(prototypes.unsqueeze(0).unsqueeze(0) - x.unsqueeze(2), dim=3)  # B, L, n_c
+        output = -torch.linalg.norm(prototypes.unsqueeze(0).unsqueeze(0) - x.unsqueeze(2), dim=3)  # B, L, n_c
         loss = self.loss_func(labels, output)
 
         if self.support_head:
@@ -108,7 +108,7 @@ class SSLProtoNetSystem(AdaptorSystem):
                 "Total Loss": loss
             }
             
-        return loss_dict, output_support
+        return loss_dict, output
 
     def training_step(self, batch, batch_idx):
         sup_batch, qry_batch, repr_info = batch[0]

lightning/systems/phoneme_recognition/heads.py
@@ -44,7 +44,7 @@ class MultilingualClusterHead(nn.Module):
             sim = F.cosine_similarity(y, x.unsqueeze(2), dim=3)  # B, L, n_c
             return sim / self.temperature
         elif self.mode == "l2":
-            sim = torch.linalg.norm(y - x.unsqueeze(2), dim=3)
+            sim = -torch.linalg.norm(y - x.unsqueeze(2), dim=3)
             return sim
         else:
             raise NotImplementedError